ad08065084ab12c0e96ba547626f85892bde4717
vSpxd <44081190+privel@users.noreply.github.com>
2024-03-01 22:52:32 +0600

    rybeshka

---
+++ ROB/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    public class Circle
+    {
+        bool isShpere;
+        public Circle(bool isShpere) { 
+            
+            this.isShpere = isShpere; // Circle or Sphere
+
+
+        }
+
+
+
+    }
+}
---
+++ ROB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using Circle;
+using Rectangle;
+
+public class Program
+{
+    public static void Main(string[] args)
+
+    {
+
+        RectangleM rec = new RectangleM(); 
+
+
+        int ans; 
+        Console.WriteLine("1 - rectangle");
+        Console.WriteLine("2 - square");
+        Console.WriteLine("3 - circle");
+        Console.WriteLine("0 - exit");
+
+        ans = Convert.ToInt16( Console.ReadLine());
+
+        switch (ans)
+        {
+            case 1:
+                rec.ifRectangle();
+                break;
+        }
+
+
+    }
+}
